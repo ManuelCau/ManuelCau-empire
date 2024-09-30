56251bae08bab97caeb32b1383de9a863463303d
Manuel Cau <caumanuel10@gmail.com>
2024-09-30 20:30:29 +0200

    banners

---
+++ FRONT-END/src/components/GraphicsBannerHome.tsx
@@ -0,0 +1,12 @@
+import graphicsImage from "../assets/IMG/LIQUIDO never meant.jpg";
+
+export function GraphicBannerHome() {
+  return (
+    <div className="g-bannerbox">
+    
+      <div className="graphics-banner-home">
+        <img src={graphicsImage} alt="img banner graphics" />
+      </div>
+    </div>
+  );
+}
--- FRONT-END/src/components/Homepage.tsx
+++ FRONT-END/src/components/Homepage.tsx
@@ -1,9 +1,19 @@
+import { CodeBannerHome } from "./CodeBannerHome";
+import { GraphicBannerHome } from "./GraphicsBannerHome";
 import { HomeBanner } from "./HomeBanner";
+import { Waves } from "./Waves";
 
-export function Homepage(){
-    return (
-        <div className="back-home">
-           <HomeBanner/>
-        </div>
-    )
+export function Homepage() {
+  return (
+    <div className="back-home">
+      <div>
+        <HomeBanner />
+        <Waves />
+      </div>
+      <div className="home-banners">
+        <GraphicBannerHome />
+        <CodeBannerHome />
+      </div>
+    </div>
+  );
 }
